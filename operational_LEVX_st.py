560f3b5b66c31339a1fccf590d676efad5af7b67
granantuin <49106818+granantuin@users.noreply.github.com>
2022-03-18 10:23:49 +0100

    Update operational_LEVX_st.py

--- operational_LEVX_st.py
+++ operational_LEVX_st.py
@@ -85,6 +85,15 @@ model_x_var=meteo_model[:24][alg["x_var"]]
 #forecast machine learning  wind direction
 skyc1_ml=alg["ml_model"].predict(model_x_var)
 
+#open new algorithm
+alg=pickle.load(open("algorithms/skyl1_LEVX_d0.al","rb"))
+
+#select x _var
+model_x_var=meteo_model[:24][alg["x_var"]]
+
+#forecast machine learning  wind direction
+skyl1_ml=alg["ml_model"].predict(model_x_var)
+
 
 #show results wind
 st.write("#### **Machine learning results wind forecast  D0**")
@@ -103,16 +112,17 @@ AgGrid(df_all)
 st.markdown(get_table_download_link(df_all),unsafe_allow_html=True)
 
 #show results prec visibility fog cloud cover
-st.write("#### **Machine learning results (precipitation, visibility, BR/FG, cloud cover) forecast D0**")
+st.write("#### **Machine learning results (precipitation, visibility, BR/FG, cloud low layer cover and height) forecast D0**")
 st.write("###### **Horizontal visibility min [T-1hour,T)**")
 st.write("###### **Precipitation on time T**")
 st.write("###### **BR or Fog on time T**")
-st.write("###### **Cloud cover on time T**")
+st.write("###### **Cloud cover and cloud height on time T**")
 df_for0=pd.DataFrame({"time UTC":meteo_model[:24].index,
                      "Horizontal visibility <=1000m (prob)":vis_ml,
-                      "Precipitation (prob)":prec_ml,
-                      "Fog or BR":brfg_ml,
-                     "Cloud cover":skyc1_ml})
+                     "Precipitation (prob)":prec_ml,
+                     "Fog or BR":brfg_ml,
+                     "Cloud cover":skyc1_ml,
+                     "Cloud height":skyl1_ml})
 
 df_all=pd.concat([df_for0.set_index("time UTC"),metar_df],axis=1).reset_index()
 df_all=df_all.rename(columns={"index": "Time UTC"})
