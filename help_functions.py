c6bcddad0119f7fc402d1a483c7f595382b43c55
granantuin <49106818+granantuin@users.noreply.github.com>
2022-11-23 11:16:29 +0100

    Update help_functions.py

--- help_functions.py
+++ help_functions.py
@@ -11,41 +11,75 @@ def get_meteogalicia_model(coorde):
     -------
     dataframe with meteeorological variables forecasted.
     """
+    try:
+        
+      #defining url to get model from Meteogalicia server
+      today=pd.to_datetime("today")
+      head1="http://mandeo.meteogalicia.es/thredds/ncss/modelos/WRF_HIST/d03"
+      head2=today.strftime("/%Y/%m/wrf_arw_det_history_d03")
+      head3=today.strftime("_%Y%m%d_0000.nc4?")
+      head=head1+head2+head3
+ 
+      var1="var=dir&var=mod&var=wind_gust&var=mslp&var=temp&var=rh&var=visibility&var=lhflx"
+      var2="&var=lwflx&var=conv_prec&var=prec&var=swflx&var=shflx&var=cape&var=cin&var=cfh&var=T850"
+      var3="&var=cfl&var=cfm&var=cft&var=HGT500&var=HGT850&var=T500&var=snow_prec&var=snowlevel"
+      var=var1+var2+var3
+ 
+      f_day=(today+timedelta(days=2)).strftime("%Y-%m-%d") 
+      tail="&time_start="+today.strftime("%Y-%m-%d")+"T01%3A00%3A00Z&time_end="+f_day+"T23%3A00%3A00Z&accept=csv"
+ 
+      dffinal=pd.DataFrame() 
+      for coor in list(zip(coorde.lat.tolist(),coorde.lon.tolist(),np.arange(0,len(coorde.lat.tolist())).astype(str))):
+          dffinal=pd.concat([dffinal,pd.read_csv(head+var+"&latitude="+str(coor[0])+"&longitude="+str(coor[1])+tail,).add_suffix(str(coor[2]))],axis=1)    
+ 
     
-    #defining url to get model from Meteogalicia server
-    today=pd.to_datetime("today")
-    head1="http://mandeo.meteogalicia.es/thredds/ncss/modelos/WRF_HIST/d03"
-    head2=today.strftime("/%Y/%m/wrf_arw_det_history_d03")
-    head3=today.strftime("_%Y%m%d_0000.nc4?")
-    head=head1+head2+head3
+      #filter all columns with lat lon and date
+      dffinal=dffinal.filter(regex='^(?!(lat|lon|date).*?)')
  
-    var1="var=dir&var=mod&var=wind_gust&var=mslp&var=temp&var=rh&var=visibility&var=lhflx"
-    var2="&var=lwflx&var=conv_prec&var=prec&var=swflx&var=shflx&var=cape&var=cin&var=cfh&var=T850"
-    var3="&var=cfl&var=cfm&var=cft&var=HGT500&var=HGT850&var=T500&var=snow_prec&var=snowlevel"
-    var=var1+var2+var3
+      #remove column string between brakets
+      new_col=[c.split("[")[0]+c.split("]")[-1] for c in dffinal.columns]
+      for col in zip(dffinal.columns,new_col):
+          dffinal=dffinal.rename(columns = {col[0]:col[1]})
  
-    f_day=(today+timedelta(days=2)).strftime("%Y-%m-%d") 
-    tail="&time_start="+today.strftime("%Y-%m-%d")+"T01%3A00%3A00Z&time_end="+f_day+"T23%3A00%3A00Z&accept=csv"
+      dffinal=dffinal.set_index(pd.date_range(start=today.strftime("%Y-%m-%d"), end=(today+timedelta(days=3)).strftime("%Y-%m-%d"), freq="H")[1:-1])
+      control = True 
+        
+    except:
+      today=pd.to_datetime("today")-timedelta(1)  
+      #defining url to get model from Meteogalicia server
+      today=pd.to_datetime("today")
+      head1="http://mandeo.meteogalicia.es/thredds/ncss/modelos/WRF_HIST/d03"
+      head2=today.strftime("/%Y/%m/wrf_arw_det_history_d03")
+      head3=today.strftime("_%Y%m%d_0000.nc4?")
+      head=head1+head2+head3
  
-    dffinal=pd.DataFrame() 
-    for coor in list(zip(coorde.lat.tolist(),coorde.lon.tolist(),np.arange(0,len(coorde.lat.tolist())).astype(str))):
-        dffinal=pd.concat([dffinal,pd.read_csv(head+var+"&latitude="+str(coor[0])+"&longitude="+str(coor[1])+tail,).add_suffix(str(coor[2]))],axis=1)    
+      var1="var=dir&var=mod&var=wind_gust&var=mslp&var=temp&var=rh&var=visibility&var=lhflx"
+      var2="&var=lwflx&var=conv_prec&var=prec&var=swflx&var=shflx&var=cape&var=cin&var=cfh&var=T850"
+      var3="&var=cfl&var=cfm&var=cft&var=HGT500&var=HGT850&var=T500&var=snow_prec&var=snowlevel"
+      var=var1+var2+var3
+ 
+      f_day=(today+timedelta(days=2)).strftime("%Y-%m-%d") 
+      tail="&time_start="+today.strftime("%Y-%m-%d")+"T01%3A00%3A00Z&time_end="+f_day+"T23%3A00%3A00Z&accept=csv"
+ 
+      dffinal=pd.DataFrame() 
+      for coor in list(zip(coorde.lat.tolist(),coorde.lon.tolist(),np.arange(0,len(coorde.lat.tolist())).astype(str))):
+          dffinal=pd.concat([dffinal,pd.read_csv(head+var+"&latitude="+str(coor[0])+"&longitude="+str(coor[1])+tail,).add_suffix(str(coor[2]))],axis=1)    
  
     
-    #filter all columns with lat lon and date
-    dffinal=dffinal.filter(regex='^(?!(lat|lon|date).*?)')
+      #filter all columns with lat lon and date
+      dffinal=dffinal.filter(regex='^(?!(lat|lon|date).*?)')
  
-    #remove column string between brakets
-    new_col=[c.split("[")[0]+c.split("]")[-1] for c in dffinal.columns]
-    for col in zip(dffinal.columns,new_col):
-        dffinal=dffinal.rename(columns = {col[0]:col[1]})
+      #remove column string between brakets
+      new_col=[c.split("[")[0]+c.split("]")[-1] for c in dffinal.columns]
+      for col in zip(dffinal.columns,new_col):
+          dffinal=dffinal.rename(columns = {col[0]:col[1]})
  
-    dffinal=dffinal.set_index(pd.date_range(start=today.strftime("%Y-%m-%d"), end=(today+timedelta(days=3)).strftime("%Y-%m-%d"), freq="H")[1:-1])  
-           
+      dffinal=dffinal.set_index(pd.date_range(start=today.strftime("%Y-%m-%d"), end=(today+timedelta(days=3)).strftime("%Y-%m-%d"), freq="H")[1:-1])  
+      control = False     
          
-    return dffinal 
+    return dffinal,control 
 
-def get_metar(oaci):
+def get_metar(oaci,control):
      """
      get metar from IOWA university database
      
@@ -56,12 +90,18 @@ def get_metar(oaci):
 
      """
      #today metar
+    
+     if control:
+       today = pd.to_datetime("today")+timedelta(1)
+       yes = today-timedelta(1)
+     else:
+        today = pd.to_datetime("today")+timedelta(1)
+        yes = today-timedelta(2)
      
-     today=pd.to_datetime("today")
      #url string
      s1="https://mesonet.agron.iastate.edu/cgi-bin/request/asos.py?station="
      s2="&data=all"
-     s3="&year1="+today.strftime("%Y")+"&month1="+today.strftime("%m")+"&day1="+today.strftime("%d")
+     s3="&year1="+yes.strftime("%Y")+"&month1="+yes.strftime("%m")+"&day1="+yes.strftime("%d")
      s4="&year2="+today.strftime("%Y")+"&month2="+today.strftime("%m")+"&day2="+today.strftime("%d")
      s5="&tz=Etc%2FUTC&format=onlycomma&latlon=no&missing=M&trace=T&direct=no&report_type=1&report_type=2"
      url=s1+oaci+s2+s3+s4+s5
